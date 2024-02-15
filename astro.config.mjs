0dd62abe750b1feb8a7cc922c1e051838aa94beb
david <daph013@gmailcom>
2024-02-15 17:31:50 -0400

    primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config 
 export default defineConfig({
-    site: 'https://profwilliamarte.github.io',
+    site: 'https://sdvrios.github.io',
     base: '/astro01',
     build: {
        assets: 'astro' /// muy importante
